45c9051377b1070521486e24e3e260d5476fdd56
Vmiruthula <67530944+Vmiruthula@users.noreply.github.com>
2020-07-18 19:23:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function summer (){
+    alert("hey ! it's summer");
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png' ) ";   
+    document.body.style.backgroundSize="100%";
+    document.body.style.backgroundRepeat= "no repeat";
+}
+function winter(){
+    alert("hey! its winter");
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundSize="100%";
+    document.body.style.backgroundRepeat= "no repeat";
+}
+function autumn(){
+    alert("hey ! its autumn"); 
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundSize="100%";
+    document.body.style.backgroundRepeat= "no repeat";
+} 
+function spring(){
+    alert("hey ! its spring");  
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundSize="100%";
+    document.body.style.backgroundRepeat= "no repeat";
+} 
+function copy(){
+    alert("lol ! you can't  copy");
+}
